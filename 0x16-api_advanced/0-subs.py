df26407d306909c1f2a0b9adf2b9c5357149e064
wallace <exnesswallace@gmail.com>
2024-03-06 00:03:52 +0200

    Write a function that queries the Reddit API and returns the number of subscribers (not active users, total subscribers

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+
+"""imports a module to retrieve info"""
+import requests
+
+
+def number_of_subscribers(subreddit):
+    """function that queries the Reddit API
+    and returns the number of subscribers"""
+
+    api_url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+
+    response = requests.get(api_url, headers={'User-Agent': 'your_user_agent'})
+
+    if response.status_code == 200:
+        subreddit_info = response.json().get('data', {}).get('subscribers', 0)
+
+        return subreddit_info
+
+    elif response.status_code == 404:
+
+        return (0)
