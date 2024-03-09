1d9be741b40fe9fdc8d0d993f01fdca7b4d27f46
wallace <exnesswallace@gmail.com>
2024-03-09 09:45:20 +0200

    custom user agent

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
 """imports a module to retrieve info"""
-
-
 import requests
 
 
@@ -9,8 +7,9 @@ def number_of_subscribers(subreddit):
     """function that queries the Reddit API
     and returns the number of subscribers"""
 
+    custom_user_agent = 'your_custom_user_agent/1.0 by Wallace'
     api_url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-    headers = {'User-Agent': 'your_user_agent'}
+    headers = {'User-Agent': custom_user_agent}
     response = requests.get(api_url, headers=headers, allow_redirects=False)
 
     if response.status_code == 404:
