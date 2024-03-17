234181e164ef78f1a71461d52242ecc23880e5d2
wallace <exnesswallace@gmail.com>
2024-03-17 05:42:03 +0200

    Make it to take more requests

---
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -0,0 +1,11 @@
+#Trying to increse the number of requests taken
+
+  exec { 'Trying_to_increase_requests':
+    command => '/bin/sed -i /etc/nginx/default/nginx'.
+    path    => 'usr/local/bin/:/bin/'
+}
+
+  exec {'Restart_ngunx':
+    command => '/etc/init.d/nginx restart',
+    path    => '/etc/init.d',
+}
