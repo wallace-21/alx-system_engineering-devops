2c7e877f677741c9df1f6f85b33a4d06e41503e9
wallace <exnesswallace@gmail.com>
2024-03-17 05:50:54 +0200

    forgot the mark

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,8 +1,8 @@
 #Trying to increse the number of requests taken
 
   exec { 'Trying_to_increase_requests':
-    command => '/bin/sed -i /etc/nginx/default/nginx'.
-    path    => 'usr/local/bin/:/bin/'
+    command => '/bin/sed -i /etc/nginx/default/nginx',
+    path    => 'usr/local/bin/:/bin/',
 }
 
   exec {'Restart_ngunx':
